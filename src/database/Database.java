0828e546a14a9a0570817b7fd4c88b451081f550
PundoJnr <pundoarthur@gmail.com>
2017-07-11 06:45:27 -0700

    initial commit

--- src/database/Database.java
+++ src/database/Database.java
@@ -2,21 +2,28 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
 
 
 public class Database {
 
    
     public static void main(String[] args) {
+        //connecting to database
         try{
-            String host="jdbc:derby://localhost:1527/123 [arthur on ARTHUR]";
-            String uName="arthur";
-            String uPass="arthur";
+            Class.forName("org.apache.derby.jdbc.ClientDriver");//database class
+            String host="jdbc:derby://localhost:1527/123 [arthur on ARTHUR]";//database URL
+            String uName="arthur";//name of database
+            String uPass="arthur";//password of database
             Connection con = DriverManager.getConnection(host,uName,uPass);
         }
-        catch(SQLException err){
+        catch(Exception err){
             System.out.println(err.getMessage());
         }
+       
+       //Write to the database 
     }
     
 }
